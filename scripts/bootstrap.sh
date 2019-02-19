36e6e19f7ae76ffa7f4dfe637e11e5f8d28f92af
pc37utn <pc37utn@users.noreply.github.com>
2019-02-19 11:17:11 -0500

    clean up bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -28,6 +28,9 @@ echo "export CANTALOUPE_LOGS="$CANTALOUPE_LOGS >> /etc/profile.d/islandora.sh
 
 source /etc/profile.d/islandora.sh
 
+# update everything
+sudo yum update
+
 # setup timezone
 sudo timedatectl set-timezone America/New_York
 
@@ -35,35 +38,28 @@ sudo timedatectl set-timezone America/New_York
 sudo sed -i 's|SELINUX=enforcing$|SELINUX=disabled|' /etc/selinux/config
 sudo touch /.autorelabel
 
-# utilities
-sudo yum -y install wget mc bzip2 zip unzip ntp psmisc
-
-# Build tools
-sudo yum -y install gcc kernel-devel kernel-headers autoconf
+# utilities and build tools
+sudo yum -y install wget mc bzip2 zip unzip ntp psmisc gcc kernel-devel kernel-headers autoconf git vim htop tree mc zsh
 
 # add epel repo
 sudo yum -y install epel-release
-#sudo yum -y install dkms
 
 # add remi repo and enable php72
 sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum -y install yum-plugin-priorities
 sudo yum-config-manager --enable remi-php72
 
-# update everything
-sudo yum update
-# add Git vim
-sudo yum -y install git vim
+
+# Lamp server
+sudo yum -y install httpd mariadb-server httpd-devel mysqlclient 
+sudo yum -y install php php-devel php-cli php-mysql php-mcrypt php-mbstring php-gd php-xml php-soap php-curl
+sudo systemctl enable mariadb
+sudo systemctl enable httpd
+sudo systemctl start mariadb
+sudo systemctl start httpd
 
 # add openjdk8 java 
 sudo yum -y install java-1.8.0-openjdk
-
-# Java 8 (Oracle)
-#wget -q --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u201-b09/jdk-8u201-linux-x64.rpm
-#rpm -Uvh jdk-8*
-#rm -f jdk-8*
-# make java 8 default with the alternatives command
-#sudo alternatives  --set java /usr/java/jdk1.8.0_201/jre/bin/java
 
 # Set JAVA_HOME variable both now and for when the system restarts
 export JAVA_HOME
@@ -73,22 +69,10 @@ echo "JAVA_HOME=$JAVA_HOME" >> /etc/profile.d/islandora.sh
 # Maven
 sudo yum -y install maven ant
 
-# Tomcat - from the epel repo
+# Tomcat - from the centos repo
 sudo yum -y install tomcat tomcat-admin-webapps
 sudo usermod -a -G tomcat vagrant
 sudo systemctl enable tomcat
-
-
-# More helpful packages
-sudo yum -y install htop tree zsh mc
-
-# Lamp server
-sudo yum -y install httpd mariadb-server httpd-devel mysqlclient 
-sudo yum -y install php php-devel php-cli php-mysql php-mcrypt php-mbstring php-gd php-xml php-soap php-curl
-sudo systemctl enable mariadb
-sudo systemctl enable httpd
-sudo systemctl start mariadb
-sudo systemctl start httpd
 
 sudo usermod -a -G apache vagrant
 sudo mysqladmin -u root password islandora

be7ff4f7c20e9340e8a48c6f6a44ff5f01335ae7
pc37utn <pc37utn@users.noreply.github.com>
2019-02-19 10:12:41 -0500

    clean up ffmpeg install

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 echo "Installing FFmpeg."
-
-SHARED_DIR=$1
-
-if [ -f "$SHARED_DIR/configs/variables" ]; then
-  . "$SHARED_DIR"/configs/variables
-fi
-
-
 # Install nux repo for ffmpeg
 sudo rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
 sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm

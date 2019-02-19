2d3e2d71d84c185ae27e37b3f44f698cf67aa6ba
pc37utn <pc37utn@users.noreply.github.com>
2019-02-19 10:08:44 -0500

    clean up tesseract install

--- scripts/tesseract.sh
+++ scripts/tesseract.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
 echo "Installing Tesseract"
-
-SHARED_DIR=$1
-
-if [ -f "$SHARED_DIR/configs/variables" ]; then
-  . "$SHARED_DIR"/configs/variables
-fi
-
-yum -y install tesseract tesseact-osd tesseract-langpack-fra
+sudo yum -y install tesseract tesseact-osd tesseract-langpack-fra

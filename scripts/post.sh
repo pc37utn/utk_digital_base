c4dcc8d5fe1d4afafd4ac6c658b7339bb1a040d3
pc37utn <pc37utn@users.noreply.github.com>
2019-02-20 14:17:29 -0500

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Setup a user for Tomcat Manager ( updated to "manager-gui")
-sudo sed -i '$i<user username="islandora" password="islandora" roles="manager-gui"/>' /etc/tomcat/tomcat-users.xml
-sudo systemctl restart tomcat
-sudo sleep 30
 sudo systemctl restart httpd
 # Set correct permissions on sites/default/files
 sudo chown -R apache.apache /vhosts/digital/web/collections/sites/default/files

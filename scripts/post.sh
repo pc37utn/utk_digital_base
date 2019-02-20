2c136ab8306e2979cef9b0a8ad609cd8e0390102
pc37utn <pc37utn@users.noreply.github.com>
2019-02-20 09:18:09 -0500

    add another httpd restart

--- scripts/post.sh
+++ scripts/post.sh
@@ -3,6 +3,7 @@
 # Setup a user for Tomcat Manager ( updated to "manager-gui")
 sed -i '$i<user username="islandora" password="islandora" roles="manager-gui"/>' /etc/tomcat/tomcat-users.xml
 systemctl restart tomcat
-
+sleep 30
+systemctl restart httpd
 # Set correct permissions on sites/default/files
-chmod -R 775 /vhosts/digital/web/collections/sites/default/files
+chown -R apache.apache /vhosts/digital/web/collections/sites/default/files

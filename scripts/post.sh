b8c4b5c06d595e27b7067bcf26115280e1f9e90a
pc37utn <pc37utn@users.noreply.github.com>
2019-02-21 14:24:12 -0500

    add cantaloupe variables

--- scripts/post.sh
+++ scripts/post.sh
@@ -3,3 +3,13 @@
 sudo systemctl restart httpd
 # Set correct permissions on sites/default/files
 sudo chown -R apache.apache /vhosts/digital/web/collections/sites/default/files
+cd /vhosts/digital/web/collections
+drush vset islandora_openseadragon_tilesource 'iiif'
+drush vset islandora_openseadragon_iiif_url 'http://localhost:8000/iiif/2'
+drush vset islandora_openseadragon_iiif_token_header 1
+drush vset islandora_openseadragon_iiif_identifier '[islandora_openseadragon:pid]~[islandora_openseadragon:dsid]~[islandora_openseadragon:token]'
+
+drush vset islandora_internet_archive_bookreader_iiif_identifier '[islandora_iareader:pid]~[islandora_iareader:dsid]~[islandora_iareader:token]'
+drush vset islandora_internet_archive_bookreader_iiif_url 'http://localhost:8000/iiif/2'
+drush vset islandora_internet_archive_bookreader_iiif_token_header 1
+drush vset islandora_internet_archive_bookreader_pagesource 'iiif'
